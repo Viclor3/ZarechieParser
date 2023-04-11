bdc027f1afad9ea72bf09fa80dd9af762a4cdc25
Calibur <hearthstonemine@mail.ru>
2023-04-11 15:20:23 +0500

    Парсинг всех товаров

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup as BS
 
 _url = 'https://msz72.ru/catalog/'
-#https://msz72.ru/catalog/gotovye-resheniya/prikhozhie-gotovye/
+
 
 def get_items_urls():
     response = requests.get(url=_url)
@@ -23,25 +23,29 @@ def get_items_urls():
 
 def get_data(file_path):
     furniture_name = []
+    clear_furniture_name = []
     with open(file_path) as file:
         urls_list = [url.strip() for url in file.readlines()]
 
     for url in urls_list:
         response = requests.get(url=url)
-        soup = BS(response.content, 'html.parser')
-
+        soup = BS(response.content, 'lxml')
         try:
-            item_name = soup.find_all('div', {'class': 'inner'})
+            item_name = soup.find('div', {'class': 'inner'}).find_all('div', {'row'})
             for item in item_name:
-                info_block = item.find('div', {'class': 'info'})
-                for furniture in info_block:
-                    name = furniture.find('a')
+                description = item.find_all('div', {'info'})
+                for href in description:
+                    name = href.find('a').getText()
                     furniture_name.append(name)
-
+                    for i in furniture_name:
+                        i = i.strip()
+                        clear_furniture_name.append(i)
         except Exception as _ex:
             item_name = None
 
-    print(furniture_name)
+    with open('catalog-items.csv', 'w', encoding='UTF-8', newline='') as file:
+        for name in clear_furniture_name:
+            file.write(f"{name}\n")
 
 
 def main():
